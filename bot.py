dc88811dab3b6db52cc2a4812e8a034940e16f90
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2022-12-19 12:15:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,11 +18,11 @@ def start(message):
 
 
 @bot.message_handler(content_types=['text', 'photo'],
-                     func=lambda message: message.chat.id != -820699648 
-                     and (message.text in subjects or message.caption in subjects))
+                     func=lambda message: message.chat.id != -820699648)
 def forward(message):
-    bot.send_message(message.chat.id, random.choice(answers))
-    bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
+    if any([i for i in message.text.split() if i in subjects or i[:-2] in subjects]):
+        bot.send_message(message.chat.id, random.choice(answers))
+        bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
 
 @server.route('/' + '5879551227:AAG2HSXqGAS592uAdkhTZqEFuiLoUXIEjXw', methods=['POST'])
 def getMessage():
