304b92de2b067476830df5380a6b2c7e213c0e3c
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2022-12-20 10:52:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ import os
 server = Flask(__name__)
 bot = telebot.TeleBot('5879551227:AAG2HSXqGAS592uAdkhTZqEFuiLoUXIEjXw')
 
-subjects = ["#алгебра", "#русский", "#литра", "#англ", "#история", "#башяз", "#башлит", "#астрономия",
-            "#алгебра/n", "#русский/n", "#литра/n", "#англ/n", "#история/n", "#башяз/n", "#башлит/n", "#астрономия/n"]
+subjects = ["#алгебра", "#русский", "#литра", "#англ", "#история", "#башяз", "#башлит", "#астрономия"]
 answers = ["хорош", "лучший", "спс"]
 
 
@@ -22,12 +21,18 @@ def start(message):
                      func=lambda message: message.chat.id != -820699648)
 def forward(message):
     print(message.text)
-    if len([i for i in message.caption.split() if i in subjects]) > 0:
-        bot.send_message(message.chat.id, random.choice(answers))
-        bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
-    if len([i for i in message.text.split() if i in subjects]) > 0:
-        bot.send_message(message.chat.id, random.choice(answers))
-        bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
+    if message.text != None:
+        for i in subjects:
+            if i in message.text:
+                bot.send_message(message.chat.id, random.choice(answers))
+                bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
+                return 
+    if message.caption != None:
+        for i in subjects:
+            if i in message.caption:
+                bot.send_message(message.chat.id, random.choice(answers))
+                bot.forward_message(-820699648, from_chat_id=message.chat.id, message_id=message.message_id)
+                return
 
 @server.route('/' + '5879551227:AAG2HSXqGAS592uAdkhTZqEFuiLoUXIEjXw', methods=['POST'])
 def getMessage():
